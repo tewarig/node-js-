e246a87fb916dee2037152204480a0655fdcaff0
Gaurav Tewari <gauravtewari111@gmail.com>
2020-07-18 15:33:51 +0530

    npm basic added

--- modules/events/app.js
+++ modules/events/app.js
@@ -1,11 +1,12 @@
+const EventEitter = require("events");
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
-
 //Register a event
-emitter.on("Messaged Logged", function () {
-  console.log("Listener Called");
+emitter.on("message logged", function (arg) {
+  //e for envt or event arg
+  console.log("Listner Called", arg);
 });
 
-emitter.emit("Message Logged");
+emitter.emit("messageLogged", { id: 1, url: "http:/" });
 //emit means making noice or emmit something
 //making singnal event has happen
---
+++ npm-demo/index.js
@@ -0,0 +1,11 @@
+var _ = require("underscore");
+//core modules
+//files
+//node_modules -- at final
+var ispresent = _.contains([1, 2, 3, 4], 4);
+
+console.log(ispresent);
+
+//npm list
+//npm list depth=0
+// npm view mongoose
